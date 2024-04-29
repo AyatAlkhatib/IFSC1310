2e4e03dd49f2fe03ab4ba6f9a80b722cdb79a8a8
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2024-04-29 19:24:25 +0000

    added js scripts

--- slick/assests/js/scripts.js
+++ slick/assests/js/scripts.js
@@ -1,7 +1,15 @@
-$('.fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
